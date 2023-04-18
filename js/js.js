37587fdb166a0f00ac686e0cbd9de21031e3a3cc
Billy <billyjag1@gmail.com>
2023-04-17 21:24:37 -0400

    fix errores de logica3

---
+++ js/js.js
@@ -0,0 +1,16 @@
+function validarCampos(){
+   var textArea =  document.getElementById("textArea").value;
+   var campoNombre =  document.getElementById("campoNombre").value;
+   var campoCorreo =  document.getElementById("campoCorreo").value;
+
+   
+   if (campoNombre=== ''){
+    alert('debe ingresar el campo nombre')
+   }
+   if (campoCorreo=== ''){
+    alert('debe ingresar el campo correo')
+   }
+   if (textArea=== ''){
+    alert('debe ingresar el campo mensaje')
+   }
+}
